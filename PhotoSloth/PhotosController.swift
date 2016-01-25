e665bb7ad803d39bb155338eb23091fa20980629
arothberg <Adam@adams-macbook-pro.local>
2016-01-24 21:17:21 -0500

    table

--- PhotoSloth/PhotosController.swift
+++ PhotoSloth/PhotosController.swift
@@ -61,7 +61,11 @@ class PhotosController: UICollectionViewController {
         }
         if let coordinates = photoAsset.location?.coordinate {
             Googles.getLocationTags(coordinates.latitude, longitude: coordinates.longitude) { tags in
-                
+                slothRealm.write {
+                    for tag in tags {
+                        asset?.tags.append(SLTag(string: tag))
+                    }
+                }
             }
         }
         cell.setup(asset!)
